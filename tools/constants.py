b4cdc308804627aae1468afedb9c5cde9d71624b
Tzeng Yuxio <tzengyuxio@gmail.com>
2022-07-27 09:19:28 +0800

    feat: (wip) find origin

--- tools/constants.py
+++ tools/constants.py
@@ -9,7 +9,11 @@ tropical_year = 365.242190
 # 《天文年鑑2017》朔望月，191頁，臺北市立天文科學教育館，2017年
 synodic_month = 29.530589
 
-# 週期 cycle
+# 週期 cycle (未命名)
 cycle_years = 4418
 cycle_months = 54643
 cycle_days = 1613640
+
+# 子輿日
+
+# 共和曆
--- tools/ziyu_day.py
+++ tools/ziyu_day.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from math import modf
+from math import modf, floor
 
 from skyfield import almanac
 from skyfield.api import load
@@ -8,10 +8,28 @@ import constants
 
 # Ephemeris: 曆書, 星曆表。
 # ref.: https://rhodesmill.org/skyfield/planets.html
-# ephemeris = "de422.bsp" # Issued in 2008, -3000 to 3000, 623 MB
-ephemeris = "de441.bsp"  # Issued in 2020, -13200 to 17191, 3.1 GB
+# ephemeris = "de422.bsp"  # Issued in 2008, -3000 to 3000, 623 MB
+# ephemeris = "de441.bsp"  # Issued in 2020, -13200 to 17191, 3.1 GB
+ephemeris = "de441_part-1.bsp"  # Issued in 2020, -13200 to 17191, 3.1 GB
+# ephemeris = "kalendaro.bsp"  # Issued in 2020, -5000 to 3000, based on de441.bsp
 
-e = load(ephemeris)
+eph = load(ephemeris)
+ts = load.timescale()
+
+
+def is_same_day(t0, t1, timezone=constants.TZ_GMT):
+    y0, m0, d0, _, _, _ = t0.tt_calendar()
+    y1, m1, d1, _, _, _ = t1.tt_calendar()
+    return (y0 == y1) and (m0 == m1) # and (d0 == d1)
+
+
+def ganzhi_of_jd(jd):
+    return (int(floor(jd + .5)) - 11) % 60
+
+
+def is_jiazi(t, delta):
+    return True
+    # return (ganzhi_of_jd_with_delta(t.tt, delta) == 0)
 
 
 def find_winter_solstice(t0, t1):
@@ -21,7 +39,7 @@ def find_winter_solstice(t0, t1):
     :param t1:
     :return:
     """
-    t, y = almanac.find_discrete(t0, t1, almanac.seasons(e))
+    t, y = almanac.find_discrete(t0, t1, almanac.seasons(eph))
     for yi, ti in zip(y, t):
         """
         0 春分 Vernal Equinox   / March Equinox
@@ -41,7 +59,7 @@ def find_new_moon(t0, t1):
     :param t1:
     :return:
     """
-    t, y = almanac.find_discrete(t0, t1, almanac.moon_phases(e))
+    t, y = almanac.find_discrete(t0, t1, almanac.moon_phases(eph))
     for yi, ti in zip(y, t):
         """
         0 新月 New Moon
@@ -55,8 +73,42 @@ def find_new_moon(t0, t1):
 
 
 def find_new_moon_winter_solstice(start_time, end_time, timezone=constants.TZ_GMT):
-    pass
+    """求朔旦冬至
+    :param start_time:
+    :param end_time:
+    :param timezone:
+    :return:
+    """
+    t, y = almanac.find_discrete(start_time, end_time, almanac.seasons(eph))
+    for yi, ti in zip(y, t):
+        if yi != 3:
+            continue
+        ti0, ti1 = ts.tt_jd(ti.tt - 1), ts.tt_jd(ti.tt + 1)
+        tnm = find_new_moon(ti0, ti1)  # time new moon, 朔日
+        if tnm is None:
+            continue
+        if not is_same_day(ti, tnm, timezone):
+            continue
+        if ganzhi_of_jd(ti.tt) != 0:
+            continue
+        y, m, d, _, _, _ = ti.tt_calendar()
+        jd = ts.utc(y, m, d).tt
+        print(y, m, d, jd, ti.utc, tnm.utc)
+    #     results.append(jd)
+    # return results
 
+def find_3034(start_time, end_time):
+    t, y = almanac.find_discrete(start_time, end_time, almanac.seasons(eph))
+    for yi, ti in zip(y, t):
+        if yi != 3:
+            continue
+        ti0, ti1 = ts.tt_jd(ti.tt - 3), ts.tt_jd(ti.tt + 3)
+        tnm = find_new_moon(ti0, ti1)  # time new moon, 朔日
+        if tnm is None:
+            continue
+        y, m, d, _, _, _ = ti.tt_calendar()
+        jd = ts.utc(y, m, d).tt
+        print(y, m, d, jd, ti.utc, tnm.utc)
 
 def find_cycle(solar_len, lunar_len, max_year=7000):
     """求週期
@@ -73,14 +125,14 @@ def find_cycle(solar_len, lunar_len, max_year=7000):
     日月時歲章蔀紀元。1元3紀4560歲, 1紀20蔀1520歲, 1蔀4章76歲, 1章19歲
 
     max_year 的預設值：
-        1. 西元前 約前5000年-前4000年：古埃及出現日曆。迄今約7000年。亦可倍求之。
+        1. 西元前 約前5000年-前4000年：古埃及出現日曆。迄今約7000年。亦可倍求之。(倍求之, 合適的有 4418,8836,13254, 最合適還是 4418)
         2. 宋祖沖之《大明曆》上元甲子，至今五萬二千一百九十算外。 元法五十九萬二千三百六十五
         3. 西元前 3200年 左右：蘇美人發明楔形文字
     """
     # 1章19歲, 1蔀76歲, 391年144閏(大明曆,祖沖之), 1紀1520歲, 1統1539年, 1元4560歲
     special_years = [19, 76, 391, 1520, 1539, 4560]
     max_time_part = 0.5  # 最後一天至少要過半天
-    print('回歸年數, 朔望月數, 太陽日數, 年循環最後一日餘時, 月循環最後一日餘時, 合甲子, 合七曜, 年循環太陽日數, 月循環太陽日數')
+    print('回歸年數, 朔望月數, 太陽日數, 年循環最後一日餘時, 月循環最後一日餘時, 合甲子, 合七曜, 年循環太陽日數, 月循環太陽日數, 年循環月循環同日')
     for i in range(max_year):
         j = round(i * solar_len / lunar_len)
         solar_days, lunar_days = i * solar_len, j * lunar_len
@@ -107,4 +159,20 @@ def find_cycle(solar_len, lunar_len, max_year=7000):
 
 if __name__ == "__main__":
     # find_new_moon_winter_solstice()
-    find_cycle(constants.tropical_year, constants.synodic_month, 10000)
+    find_cycle(constants.tropical_year, constants.synodic_month, 20000)
+    # 以下找不到合甲子的
+    # find_period(tropical_year_jacobs, synodic_month_jacobs, 14000)
+
+    print('\n\n----------------------------------------\n\n')
+
+    t0 = ts.utc(-3200, 1, 1)
+    t1 = ts.utc(1800, 12, 31)
+    # t1 = ts.utc(2999, 12, 31)
+    find_new_moon_winter_solstice(t0, t1)
+
+
+
+
+    # find_3034(ts.utc(-3040, 1, 1), ts.utc(-3030, 1, 1))
+    # -3039 12 19 611440.5004882407 CalendarTuple(year=-3039, month=12, day=19, hour=22, minute=59, second=19.840786174296227) CalendarTuple(year=-3039, month=12, day=19, hour=3, minute=9, second=13.946844205616799)
+    # -3031 12 19 614362.5004882407 CalendarTuple(year=-3031, month=12, day=19, hour=21, minute=34, second=9.670948177932587) CalendarTuple(year=-3031, month=12, day=20, hour=10, minute=44, second=36.86352962291858)
